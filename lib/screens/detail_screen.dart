a5b9e3a73f5647c47e854320d9c0cf1d0ef2b2ea
dearsecret <dearsecret1217@gmail.com>
2022-12-14 18:07:50 +0900

    detail & episodes

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -37,33 +37,108 @@ class _DetailScreenState extends State<DetailScreen> {
           style: const TextStyle(fontSize: 22),
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 15,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 35,
+            vertical: 30,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Column(
             children: [
-              Hero(
-                tag: widget.id,
-                child: Container(
-                    width: 250,
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.3),
-                            offset: const Offset(10, 10),
-                            blurRadius: 10,
-                          )
-                        ]),
-                    child: Image.network(widget.thumb)),
+              const SizedBox(
+                height: 20,
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Hero(
+                    tag: widget.id,
+                    child: Container(
+                        width: 250,
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.3),
+                                offset: const Offset(10, 10),
+                                blurRadius: 10,
+                              )
+                            ]),
+                        child: Image.network(widget.thumb)),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              FutureBuilder(
+                future: detail,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      children: [
+                        Text(snapshot.data!.about),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text("${snapshot.data!.genre}/${snapshot.data!.age}")
+                      ],
+                    );
+                  }
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      CircularProgressIndicator(),
+                    ],
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              FutureBuilder(
+                future: episodes,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      children: [
+                        for (var episode in snapshot.data!)
+                          Container(
+                            margin: const EdgeInsets.only(bottom: 5),
+                            clipBehavior: Clip.hardEdge,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.black.withOpacity(0.85)),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 10),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    episode.title,
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                  Icon(
+                                    Icons.chevron_right_outlined,
+                                    color: Colors.yellow.shade800,
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                      ],
+                    );
+                  }
+                  return const Text("...");
+                },
+              )
             ],
           ),
-        ],
+        ),
       ),
     );
   }
